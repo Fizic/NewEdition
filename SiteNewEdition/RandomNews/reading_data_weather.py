7aecebc7744aa48fd55920c99e0b8ae26cdea20e
Firsov Kirill <37708382+Fizic@users.noreply.github.com>
2020-11-28 15:52:55 +0500

    Update reading_data_weather.py

--- SiteNewEdition/RandomNews/reading_data_weather.py
+++ SiteNewEdition/RandomNews/reading_data_weather.py
@@ -2,7 +2,7 @@ import requests
 
 
 def take_data():
-    key_api = '08ae9239449049aed5d6d40d9b9ad307'
+    key_api = ''
     url = 'http://api.openweathermap.org/data/2.5/weather?q={}&appid=' + key_api
     city = 'London'
     data = requests.get(url.format('london')).json()
